edffb5648a217910d0f55e1692589176948541e9
Riyam224 <cs50riyam211#gmail.com>
2024-04-07 23:03:33 +0300

    complete task

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:task8/views/home_view.dart';
-import 'package:task8/views/prayer_view.dart';
 
 void main() {
   runApp(const PrayerTime());
@@ -11,7 +12,7 @@ class PrayerTime extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeView(),
     );
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,208 +1,127 @@
-// ignore_for_file: sized_box_for_whitespace, avoid_unnecessary_containers, prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  // todo
+  final List<Map<String, String>> prayerTimes = [
+    {'name': 'Fajr', 'time': '4:17 AM'},
+    {'name': 'Dhuhr', 'time': '12:05 PM'},
+    {'name': 'Asr', 'time': '3:40 PM'},
+    {'name': 'Maghrib', 'time': '6:28 PM'},
+    {'name': 'Isha', 'time': '8:00 PM'},
+  ];
+  final String currentPrayer;
+  final String currentTime = DateTime.now().toString().substring(11, 19);
+
+  HomeView({
+    Key? key,
+  })  : currentPrayer = _getCurrentPrayer(),
+        super(key: key);
+
+  static String _getCurrentPrayer() {
+    final DateTime now = DateTime.now();
+    if (_isSameTime(now, TimeOfDay(hour: 5, minute: 0))) {
+      return 'Fajr';
+    } else if (_isSameTime(now, TimeOfDay(hour: 12, minute: 0))) {
+      return 'Dhuhr';
+    } else if (_isSameTime(now, TimeOfDay(hour: 15, minute: 0))) {
+      return 'Asr';
+    } else if (_isSameTime(now, TimeOfDay(hour: 18, minute: 0))) {
+      return 'Maghrib';
+    } else if (_isSameTime(now, TimeOfDay(hour: 20, minute: 0))) {
+      return 'Isha';
+    } else {
+      return 'No prayer at this time';
+    }
+  }
+
+  static bool _isSameTime(DateTime dateTime, TimeOfDay timeOfDay) {
+    return dateTime.hour == timeOfDay.hour &&
+        dateTime.minute == timeOfDay.minute;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: Image.asset(
-              'assets/images/pexels-zukiman-mohamad-394355.jpg',
-              fit: BoxFit.cover,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/pexels-francesco-ungaro-2325271.jpg',
             ),
+            fit: BoxFit.cover,
           ),
-          // todo
-          Positioned(
-            top: 100,
-            left: 150,
-            right: 150,
-            child: Container(
-                child: Column(
+        ),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 50.0,
+              right: 50.0,
+            ),
+            child: Column(
               children: [
+                SizedBox(
+                  height: 100,
+                ),
                 Text(
-                  '8:22:18 PM',
+                  currentTime,
                   style: TextStyle(
-                    color: Color(0xff39565e),
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  currentPrayer,
+                  style: TextStyle(
+                    color: Colors.white,
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                Text(
-                  'fajr',
-                  style: TextStyle(
-                    color: Color(0xff39565e),
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: prayerTimes.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                          title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '${prayerTimes[index]['name']} ',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            ' ${prayerTimes[index]['time']}',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ));
+                    },
                   ),
                 ),
               ],
-            )),
+            ),
           ),
-          Positioned(
-              top: 200,
-              left: 56,
-              child: Center(
-                child: Container(
-                  height: 300,
-                  width: 400,
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'fajr',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                            Text(
-                              '4.00 am',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'fajr',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                            Text(
-                              '4.00 am',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'fajr',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                            Text(
-                              '4.00 am',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'fajr',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                            Text(
-                              '4.00 am',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'fajr',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                            Text(
-                              '4.00 am',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'fajr',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                            Text(
-                              '4.00 am',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                      ],
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Color(0xff39565e),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ))
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
